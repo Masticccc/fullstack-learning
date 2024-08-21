6adf437f543eceeba881c1c499d552e96ec065f1
John Tran <tranhuykhoi2000@gmail.com>
2024-08-21 21:01:52 +1000

    Add week 23 , week 24

---
+++ week-24/react-html/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AdvancedTasks from './components/AdvancedTasks.jsx';
+
+function App() {
+  const items = [
+    { content: 'Go for a walk', completed: false },
+    { content: 'Have a good 1hour nap', completed: true },
+    { content: 'Game whole night', completed: true },
+  ];
+
+  return (
+    <div className="container">
+      <Header abc="123 ahioasdioajioio" item={100}>
+        <div>
+          <h2>Just another header</h2>
+          <h4>ajsdiojasoidjio</h4>
+          <h6>sjaiodjioajsiodjiosd</h6>
+        </div>
+      </Header>
+      <Header>
+        <div>abc</div>
+      </Header>
+      <Header>
+        <Tasks>
+          <ul>
+            <li>Had a good diet meal</li>
+            <li>Completed today coding class</li>
+          </ul>
+        </Tasks>
+      </Header>
+      <div>
+        <Tasks>
+          <ul>
+            <li>Had a good diet meal</li>
+            <li>Completed today coding class</li>
+          </ul>
+        </Tasks>
+      </div>
+      <div>
+        <AdvancedTasks items={items} />
+      </div>
+      <div>
+        <AdvancedTasks items={[]} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
