eb40e0f13dd5070a0f4cdaf3822d74e6aafb5e70
John Tran <tranhuykhoi2000@gmail.com>
2024-10-24 19:38:18 +1100

    WIP

---
+++ practice-vault-react-router/src/routes/Root/index.jsx
@@ -0,0 +1,16 @@
+import { Link, Outlet } from 'react-router-dom';
+import './Root.css';
+
+function Root(props) {
+  return (
+    <div className="root">
+      <div className="root-item-list">
+        <Link to="/item-1">Item 1</Link>
+        <Link to="/item-2">Item 2</Link>
+      </div>
+      <div className="root-content"></div>
+    </div>
+  );
+}
+
+export default Root;
